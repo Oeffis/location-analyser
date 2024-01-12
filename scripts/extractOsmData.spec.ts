2d932599a685662060b297946d01b04bd7d2f6d1
Tim Vahlbrock <tim@vahlbrock.de>
2024-01-12 18:41:07 +0100

    Scripts: extractOsmData: add bus route with roundabot to test

--- scripts/extractOsmData.spec.ts
+++ scripts/extractOsmData.spec.ts
@@ -2,11 +2,12 @@ import { expect, test } from "vitest";
 import { OsmExtractor } from "./extractOsmData";
 
 test("extract RB 43 to Dorsten", async () => {
-    const RB43ToDorsten = 1998588;
+    const RB43ToDorsten = 1998588; // normal railway route, completely contained
+    const Bus390LindenToHerne = 16335332; // bus route, contains roundabouts
     const extractor = new OsmExtractor();
 
     const data = await extractor.getTransformed({
-        relations: [RB43ToDorsten]
+        relations: [RB43ToDorsten, Bus390LindenToHerne]
     });
 
     expect(data).toMatchSnapshot();
