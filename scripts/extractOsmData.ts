4410a3fa3ce8fcd1cfb4403df6e74901373ac1d1
Tim Vahlbrock <tim@vahlbrock.de>
2024-01-19 10:07:21 +0100

    Scripts: extractOsmData: improve structure of invocation

--- scripts/extractOsmData.ts
+++ scripts/extractOsmData.ts
@@ -1,12 +1,16 @@
 import { OsmExtractor } from "./osmExtractor";
 import { OsmTransformer } from "./osmTransformer";
 
-console.log("Extracting OSM data");
-new OsmExtractor()
-    .extract()
-    .then(result => {
-        const transformer = new OsmTransformer(result);
-        return transformer.writeToFile();
-    })
-    .then(() => console.log("Done"))
-    .catch(console.error);
+async function run(): Promise<void> {
+    console.log("Extracting OSM data");
+    const extractor = new OsmExtractor();
+    const extraction = await extractor.extract();
+
+    console.log("Done extracting, transforming");
+    const transformer = new OsmTransformer(extraction);
+    await transformer.writeToFile();
+
+    console.log("Done");
+}
+
+run().catch(console.error);
