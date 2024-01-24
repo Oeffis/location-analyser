a43ee2a51af223b628061c26a3affeb220d63663
Tim Vahlbrock <tim@vahlbrock.de>
2024-01-24 15:44:50 +0100

    Lib: add scenario tag '@slow' and corresponding profile 'slow'

--- location-analyzer/cucumber.mjs
+++ location-analyzer/cucumber.mjs
@@ -1,11 +1,21 @@
 import { register } from "node:module";
 import { pathToFileURL } from "node:url";
 
-export default [
+const defaultArgs = [
     `--format-options '{"snippetInterface": "synchronous"}'`,
     `--import features/stepDefinitions/**/*.ts`,
     `--import features/world.ts`,
     `--tags "not @ignore"`
+]
+
+export default [
+    ...defaultArgs,
+    `--tags "not @slow"`
+].join(" ");
+
+export const slow = [
+    ...defaultArgs,
+    `--tags "@slow"`
 ].join(" ");
 
 // eslint-disable-next-line no-undef
