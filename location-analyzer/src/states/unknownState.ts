aa328545ed53d3f4f3180009cbeceffe8becb318
Tim Vahlbrock <tim@vahlbrock.de>
2024-02-02 18:23:52 +0100

    Lib: remove superflous 'getNext' implementation from unknownState

--- location-analyzer/src/states/unknownState.ts
+++ location-analyzer/src/states/unknownState.ts
@@ -1,6 +1,6 @@
 import { Buffer } from "../buffer.js";
 import { DistanceCalculator, POIWithDistance } from "../distanceCalculator.js";
-import { FilledState, GeoPosition, ResultStatus, RouteState, StopState, isRouteDistance, isStopDistance } from "./states.js";
+import { FilledState, GeoPosition, ResultStatus } from "./states.js";
 
 export class UnknownState extends FilledState {
     public constructor(
@@ -10,50 +10,5 @@ export class UnknownState extends FilledState {
         location: GeoPosition
     ) {
         super(fullHistory, history, distanceCalculator, location, []);
-    }
-
-    public getNext(location: GeoPosition): FilledState {
-        const closestPois = this.distanceCalculator.getUniquePOIsNear(location);
-        this.fullHistory.append(closestPois);
-
-        const closesByAveraged = this.getClosestByAveragedDistance(closestPois);
-        const firstPoi = closesByAveraged[0];
-        if (!firstPoi) {
-            return new UnknownState(
-                this.fullHistory,
-                this.history,
-                this.distanceCalculator,
-                location
-            );
-        }
-
-        const anyIsStop = isStopDistance(firstPoi.guess);
-        if (anyIsStop) {
-            return new StopState(
-                this.fullHistory,
-                this.history,
-                this.distanceCalculator,
-                location,
-                closesByAveraged.map(guess => guess.guess).filter(isStopDistance)
-            );
-        }
-
-        if (location.speed > this.onRouteSpeedCutoff) {
-            return new RouteState(
-                this.fullHistory,
-                this.history,
-                this.distanceCalculator,
-                location,
-                closesByAveraged.map(guess => guess.guess).filter(isRouteDistance),
-                closesByAveraged.map(guess => guess.guess).filter(isRouteDistance)
-            );
-        }
-
-        return new UnknownState(
-            this.fullHistory,
-            this.history,
-            this.distanceCalculator,
-            location
-        );
     }
 }
