d518b69cfe0fb2c346ce6b7fa8adb0a25b915b7b
Tim Vahlbrock <tim@vahlbrock.de>
2024-02-02 18:12:28 +0100

    Lib: make filledState abstract

--- location-analyzer/src/states/filledState.ts
+++ location-analyzer/src/states/filledState.ts
@@ -1,8 +1,8 @@
 import { Buffer } from "../buffer.js";
 import { DistanceCalculator, POIWithDistance, StopWithDistance } from "../distanceCalculator.js";
-import { GeoPosition, ResultStatus, RouteState, State, StopState, byProximity, isCloserThan, isRouteDistance, isStopDistance } from "./states.js";
+import { GeoPosition, ResultStatus, State } from "./states.js";
 
-export class FilledState extends State implements ResultStatus {
+export abstract class FilledState extends State implements ResultStatus {
     public constructor(
         fullHistory: Buffer<POIWithDistance[]>,
         history: Buffer<ResultStatus>,
@@ -12,30 +12,6 @@ export class FilledState extends State implements ResultStatus {
     ) {
         super(fullHistory, history, distanceCalculator, guesses);
         this.history.append(this);
-    }
-
-    public getNext(location: GeoPosition): FilledState {
-        const pois = this.distanceCalculator.getUniquePOIsNear(location);
-        this.fullHistory.append(pois);
-        const rightDirectionPois = pois.filter(this.directionFilter(location));
-
-        const reSeenPoints = this.getClosestByAveragedDistance(rightDirectionPois).map(guess => guess.guess);
-
-        const guesses = rightDirectionPois
-            .filter(isCloserThan(location.accuracy))
-            .sort(byProximity);
-
-        if (reSeenPoints.length > 0) {
-            if (reSeenPoints.every(isRouteDistance)) {
-                return new RouteState(this.fullHistory, this.history, this.distanceCalculator, location, reSeenPoints, reSeenPoints);
-            } else if (reSeenPoints.every(isStopDistance)) {
-                return new StopState(this.fullHistory, this.history, this.distanceCalculator, location, reSeenPoints);
-            } else {
-                throw new Error("Mixed re-seen points");
-            }
-        }
-
-        return new FilledState(this.fullHistory, this.history, this.distanceCalculator, location, guesses);
     }
 
     public get nearbyPlatforms(): StopWithDistance[] {
